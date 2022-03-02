d1da3868a185c9a156ab8dfe562de8ce2e584641
Patryk Okpara <patrykokpara@hotmail.com>
2022-03-02 16:08:30 +0000

    Created a top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,10 @@ describe Oystercard do
   oystercard = Oystercard.new
   it 'new oystercard balance is 0' do
     expect(subject.balance).to eq 0
-  end  
+  end
+
+  it 'increases the current amount in the oystercard' do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
 end
